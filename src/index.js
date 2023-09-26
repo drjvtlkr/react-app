59394566cce882b0d6d4a7d13e776834ab70ecd0
Dheeraj Vithalkar <dheerajrvithalkar@gmail.com>
2023-09-26 12:21:06 +0530

    commenting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-function Greeting() {
-  return <h2>My First Component</h2>;
+const Greeting = () => {
+  return (
+    <React.Fragment>
+      <div>
+        <div>
+          <h3>hello world</h3>
+          <ul>
+            <li>
+              <a href="#">dancing dogs</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <h1>
+        <p>party karenge</p>
+      </h1>
+    </React.Fragment>
+    // we can use this type of React.Frgament to enclose or just go with plain old '<></>' type of styling
+  )
 }
+// function Greeting() {
+//   return React.createElement('h2', {}, 'hello world')
+// }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting />);
+root.render(<Greeting />)
