3601556a91add43e037b32125e31bbb1a82bd386
Dheeraj Vithalkar <dheerajrvithalkar@gmail.com>
2023-09-27 11:16:03 +0530

    made updates

--- src/index.js
+++ src/index.js
@@ -3,28 +3,44 @@ import ReactDOM from 'react-dom/client'
 
 import './style.css'
 
+const firstBook = {
+  author: 'Jordan Petersen',
+  title: '12 Rules For Life',
+  img: 'https://m.media-amazon.com/images/I/71OVB8HknWL._SL1500_.jpg',
+}
+
+const secondBook = {
+  author: 'Hector Garcia and Francesc Miralles',
+  title: 'Ikigai',
+  img: 'https://m.media-amazon.com/images/I/814L+vq01mL._SL1500_.jpg',
+}
+// const title = 'Atomic Habits'
+// const author = 'James XClear'
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   )
 }
-const author = 'James XClear'
-const Book = () => {
-  const title = 'Atomic Habits'
+
+const Book = (props) => {
+  console.log(props)
+  const { author, title, img } = props
   return (
     <article className="book">
-      <img
-        // className="book"
-        src="https://m.media-amazon.com/images/I/91bYsX41DVL._SL1500_.jpg"
-        alt="Atomic Habits"
-      />
+      <img src={props.img} alt={props.title} />
       <h2>{title}</h2>
-      <h4>{author}</h4>
+      <h4> {author} </h4>
     </article>
   )
 }
